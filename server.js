bea3993fcb8861d7d87519aba2309e0cb86720e4
Niyi Aromokeye <oyekeye@hotmail.com>
2019-12-24 13:22:49 +0100

    adding heroku port

--- server.js
+++ server.js
@@ -129,6 +129,6 @@ app.put('/image', (req, res)=>{
     
 })
 
-app.listen(3000, ()=>{
-    console.log('Our app is listening on port 3000')
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`Our app is listening on port ${process.env.PORT}`)
 }) 
